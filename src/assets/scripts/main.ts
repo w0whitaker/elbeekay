afb95e971ffb0ead96064e71b9a8fb0f06863ce4
William Whitaker <william@williamwhitaker.dev>
2023-05-08 17:44:52 -0400

    styles footer

--- src/assets/scripts/main.ts
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-document.querySelector<HTMLDivElement>('#app')!.insertAdjacentHTML("beforeend", `<p>maybe some dynamically generated content.</p>`);
