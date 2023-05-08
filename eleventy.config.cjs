b33cb142019a30426bb947e64fea674397763779
William Whitaker <william@williamwhitaker.dev>
2023-05-08 12:29:40 -0400

    clean up

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -3,7 +3,10 @@ const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyVitePlugin, {
-    appType: "mpa",
+    viteOptions: {
+      clearScreen: true,
+      appType: "mpa",
+    },
   });
 
   eleventyConfig.addPassthroughCopy("./src/assets");
