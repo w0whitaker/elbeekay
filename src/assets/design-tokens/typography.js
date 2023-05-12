79b7d3fe67a75db83d198671aacafcf2f9773e7a
William Whitaker <william@williamwhitaker.dev>
2023-05-12 10:12:18 -0400

    adds sidebar

--- src/assets/design-tokens/typography.js
+++ src/assets/design-tokens/typography.js
@@ -2,7 +2,14 @@ module.exports = {
   theme: {
     extend: {
       fontSize: {
-        "2xl": "5rem",
+        xs: "0.63rem",
+        sm: "0.78rem",
+        base: "0.88rem",
+        lg: "15.75px",
+        xl: "1.11rem",
+        "2xl": "1.25rem",
+        "3xl": "1.4rem",
+        "4xl": "1.58rem",
       },
     },
   },
